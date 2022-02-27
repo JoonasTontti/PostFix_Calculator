a756cba240a6be8cf0dee1ff4601159f8d37b4a3
Joonas Tontti <joonas.tontti@tuni.fi>
2022-02-27 13:07:54 +0200

    Version 1.1 complete

---
+++ stack.h
@@ -0,0 +1,31 @@
+#ifndef _STACK_H
+#define _STACK_H
+
+#include "Utility.h"
+
+// Section 2.2:
+
+typedef double Stack_entry;
+
+const int maxstack = 100;   //  small value for testing
+
+class Stack {
+public:
+	Stack();
+	Error_code pop();
+	Error_code push(const Stack_entry& item);
+	Error_code top(Stack_entry& item) const;
+
+	bool empty() const;
+	void clear(); // Reset the stack to be empty.
+	bool full() const; // If the stack is full, return true; else return false.
+	int size() const; // Return the number of entries in the stack.
+
+	friend Error_code copy_stack3( Stack &dest, Stack &source);
+
+private:
+	int count;
+	Stack_entry entry[maxstack];
+};
+
+#endif
